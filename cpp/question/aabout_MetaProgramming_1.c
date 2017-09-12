d6bf9dc5c6ef7cb8cf22df4657a59d8fdb75739d
yangyangyang <253321787@qq.com>
2017-09-12 14:37:25 +0800

    Create aabout_MetaProgramming_1.c

---
+++ cpp/question/aabout_MetaProgramming_1.c
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+constexpr int ipow(int x, int n)
+{
+	return n > 0 ? (x * ipow(x, n - 1)) : 1;
+}
+
+template <char c> constexpr int b3_helper()
+{
+	static_assert(c < '3', "not a ternary digit");
+	return c;
+}
+
+template <char c, char... args> constexpr int b3_helper()
+{
+	static_assert(c < '3', "not a ternary digit");
+	return (ipow(3, sizeof... (args)) * (c - '0')
+		/*+ b3_helper<args...>()*/); //error C2668 Why???
+}
+
+template <char... args> int operator"" _b3()
+{
+	int temp = b3_helper<args...>();
+	std::cout << temp << endl;
+	return temp;
+}
+
+int main()
+{
+	int test = 201_b3;
+	std::cout << test << endl;
+}
